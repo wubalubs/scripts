acc65c7bfff1b4f50240e94b573ef1c5e76bfe5e
David Massengale <dmgale@outlook.com>
2016-08-24 00:10:33 -0700

    housekeeping updates

--- bash/housekeeping.sh
+++ bash/housekeeping.sh
@@ -6,5 +6,6 @@
 
 LOG_DATE=$(date)
 
-find /home/david/Downloads/* -type f || echo "$LOG_DATE - No deletions" >> /var/log/deletions
-
+find /home/david/Downloads/* -mtime 20 -type f || echo "$LOG_DATE - No deletions" >> /var/log/deletions
+# the below might be better in a backup script
+# curl -X POST http://textbelt.com/text -d number=$CELL -d "message=$LOG_DATE - this is a test"
