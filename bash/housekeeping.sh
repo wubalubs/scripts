2e81d68bfb26929bba34092e1117ab420973d626
David Massengale <dmgale@outlook.com>
2016-08-25 16:14:44 -0700

    housekeeping updates

--- bash/housekeeping.sh
+++ bash/housekeeping.sh
@@ -1,4 +1,4 @@
-	#!/bin/bash
+#!/bin/bash
 #find /home/david/Downloads/* -mtime +30 -exec rm {} \;
 #We can log this by piping the find command to a logfile first
 #add text alert as well w/ textbelt.com
@@ -9,3 +9,6 @@ LOG_DATE=$(date)
 find /home/david/Downloads/* -mtime 20 -type f || echo "$LOG_DATE - No deletions" >> /var/log/deletions
 # the below might be better in a backup script
 # curl -X POST http://textbelt.com/text -d number=$CELL -d "message=$LOG_DATE - this is a test"
+# maybe this script should run a backup of homedir as well...?
+# clear cookies, etc, older than 60 days
+# general "housekeeping" as implied
