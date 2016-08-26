a2db7c1e66f126d95195c2c413d826eba8cf8899
David Massengale <dmgale@outlook.com>
2016-08-25 17:03:54 -0700

    housekeeping updates

--- bash/housekeeping.sh
+++ bash/housekeeping.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-#find /home/david/Downloads/* -mtime +30 -exec rm {} \;
-#We can log this by piping the find command to a logfile first
-#add text alert as well w/ textbelt.com
-#
 
+
+# Variables
 LOG_DATE=$(date)
+FILES="$(find /home/david/Downloads/* -mtime 20 -type f)"
+# to echo this use "${FILES}"
 
+
+# Finds files older than 20 days and deletes, reports "no deletions" if no match
 find /home/david/Downloads/* -mtime 20 -type f || echo "$LOG_DATE - No deletions" >> /var/log/deletions
+
+
+
+
+
+# find /home/david/Downloads/* -mtime +30 -exec rm {} \;
 # the below might be better in a backup script
 # curl -X POST http://textbelt.com/text -d number=$CELL -d "message=$LOG_DATE - this is a test"
 # maybe this script should run a backup of homedir as well...?
 # clear cookies, etc, older than 60 days
-# general "housekeeping" as implied
+
+
