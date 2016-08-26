512a82925aaace23005a3d98e25805e122e31bd6
David Massengale <dmgale@outlook.com>
2016-08-26 01:07:37 -0700

    housekeeping updates

--- bash/housekeeping.sh
+++ bash/housekeeping.sh
@@ -3,24 +3,22 @@
 
 # Variables
 LOG_DATE=$(date)
-FILES="$(find /home/david/Downloads/* -mtime 20 -type f)"
-# to echo this use "${FILES}"
+FILES="$(find /home/david/Downloads/* -mtime +5 -type f)"
 
-# echo files to be deleted to logfile
-printf "\\n$LOG_DATE - Deleted Items \\n${FILES}" >> ~/.deletions.log
+
+if [ -z "$FILES" ];
+
+	then	
+	printf "\\n$LOG_DATE - No Deletions\\n" >> ~/.deletions.log
+
+	else
+	printf "\\n$LOG_DATE - Deleted Items \\n${FILES}" >> ~/.deletions.log
+	find /home/david/Downloads/* -type f -mtime +5 -exec rm {} \;	
+fi
+
+
 
 
 # Finds files older than 20 days and deletes, reports "no deletions" if no match
-find /home/david/Downloads/* -mtime 20 -type f || printf "\\n$LOG_DATE - No deletions\\n" >> ~/.deletions.log
-
-
-
-
-
+# find /home/david/Downloads/* -mtime 20 -type f || printf "\\n$LOG_DATE - No deletions\\n" >> ~/.deletions.log
 # find /home/david/Downloads/* -mtime +30 -exec rm {} \;
-# the below might be better in a backup script
-# curl -X POST http://textbelt.com/text -d number=$CELL -d "message=$LOG_DATE - this is a test"
-# maybe this script should run a backup of homedir as well...?
-# clear cookies, etc, older than 60 days
-
-
