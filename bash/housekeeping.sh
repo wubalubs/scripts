c3abd6412bbb168e573ccd4d72eafabed88e9fcd
David Massengale <dmgale@outlook.com>
2016-08-26 01:34:39 -0700

    housekeeping updates

--- bash/housekeeping.sh
+++ bash/housekeeping.sh
@@ -3,7 +3,7 @@
 
 # Variables
 LOG_DATE=$(date)
-FILES="$(find /home/david/Downloads/* -mtime +5 -type f)"
+FILES="$(find /home/david/Downloads/* -mtime +10 -type f)"
 
 
 if [ -z "$FILES" ];
@@ -12,13 +12,6 @@ if [ -z "$FILES" ];
 	printf "\\n$LOG_DATE - No Deletions\\n" >> ~/.deletions.log
 
 	else
-	printf "\\n$LOG_DATE - Deleted Items \\n${FILES}" >> ~/.deletions.log
-	find /home/david/Downloads/* -type f -mtime +5 -exec rm {} \;	
+	printf "\\n$LOG_DATE - Deleted Items \\n${FILES}\\n" >> ~/.deletions.log
+	find /home/david/Downloads/* -type f -mtime +10 -exec rm {} \;	
 fi
-
-
-
-
-# Finds files older than 20 days and deletes, reports "no deletions" if no match
-# find /home/david/Downloads/* -mtime 20 -type f || printf "\\n$LOG_DATE - No deletions\\n" >> ~/.deletions.log
-# find /home/david/Downloads/* -mtime +30 -exec rm {} \;
